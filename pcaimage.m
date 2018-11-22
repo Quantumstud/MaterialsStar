415e2ff4a468ef7dbd9f30ffd56366fc2e7c1e64
Quantumstud <tara.13.1996@gmail.com>
2018-11-22 15:56:58 +0800

    Third Commit. Changes to the PCA script

--- pcaimage.m
+++ pcaimage.m
@@ -1,9 +1,9 @@
 clear;
 clc;
-load('Simulated_3.mat');
-I=Simulated_3;
+load('S5Image.mat');
+I=ImG;
 [u1,s1,v1]=svd(I);
-Blocksize=2;
+Blocksize=1;
 s1(Blocksize+1:28,Blocksize+1:92)=0;
 VT1=v1';
 Output=u1(:,1:Blocksize)*s1(1:Blocksize,1:Blocksize)*VT1(1:Blocksize,:);
@@ -14,12 +14,12 @@ newsubs=I-Output;
 % figure,imagesc(newsubs);
 % title('Substraction without equalising');
 %%
-OutPSNR=psnr(Output,I);
-SubPSNR=psnr(newsubs,I);
-stdOutput=std(Output(:));
-stdnewsub=std(newsubs(:));
-avgOutput=mean(Output(:));
-avgnewsub=mean(newsubs(:));
+% OutPSNR=psnr(Output,I);
+% SubPSNR=psnr(newsubs,I);
+% stdOutput=std(Output(:));
+% stdnewsub=std(newsubs(:));
+% avgOutput=mean(Output(:));
+% avgnewsub=mean(newsubs(:));
 %%Gauss
 pcagauss=imgaussfilt(Output,0.625);
 error_gauss=imgaussfilt(newsubs,0.625);
@@ -46,3 +46,33 @@ colorbar;
 % figure,imagesc(sub);
 % title('Substraction of normalised HAADF fft and normalised pca4 fft');
 % colorbar;
+%% Edge Detection
+[BW,Threshold]=edge(error_gauss);
+
+%% Centroidfind
+[Centroid,num]=centroidfind(BW);
+%% Specify the edge snip
+up=4;
+down=4;
+left=4;
+right=4;
+[m,n,~]=size(error_gauss);
+upstart=up;
+downend=m-down;
+leftstart=left;
+rightend=n-4;
+snippedresidual=error_gauss(upstart:downend,leftstart:rightend);
+clear -vars i
+snippedcentroid=zeros(1,2);
+newnum=0;
+for i=1:num
+    if(Centroid(i,1)>upstart && Centroid(i,1)<downend)
+        if(Centroid(i,2)>leftstart && Centroid(i,2)<rightend)
+            newnum=newnum+1;
+            snippedcentroid(newnum,:)=[Centroid(i,1)-up,Centroid(i,2)-down];
+            
+        end
+    end
+end
+num=newnum;
+clear -vars newnum up down left right i;
